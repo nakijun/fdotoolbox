61ca3d1aea4b6783189429fccfdb11df57614b25
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-07-23 10:22:15 +0000

    Fix Issue 40: Allow Save As when modifying SDF schema
    Schema management tab now has a save as button with two options:
    1) to XML - Saves the selected schema definition out to an XML file.
    2) to a new SDF - Applies the selected schema definition out to a new SDF file.
    
    And to go one further, this (should) work for *any* provider, not just SDF.

--- trunk/FdoToolbox.Core/Controls/SchemaMgrCtl.Designer.cs
+++ trunk/FdoToolbox.Core/Controls/SchemaMgrCtl.Designer.cs
@@ -34,6 +34,9 @@ namespace FdoToolbox.Core.Controls
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.btnAddSchema = new System.Windows.Forms.ToolStripButton();
             this.btnDeleteSchema = new System.Windows.Forms.ToolStripButton();
+            this.btnSaveSchema = new System.Windows.Forms.ToolStripDropDownButton();
+            this.asXMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asNewSDFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.lstClasses = new System.Windows.Forms.ListBox();
             this.toolStrip2 = new System.Windows.Forms.ToolStrip();
@@ -72,7 +75,7 @@ namespace FdoToolbox.Core.Controls
             // 
             this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
             this.splitContainer1.Size = new System.Drawing.Size(485, 248);
-            this.splitContainer1.SplitterDistance = 176;
+            this.splitContainer1.SplitterDistance = 220;
             this.splitContainer1.TabIndex = 0;
             // 
             // groupBox1
@@ -82,7 +85,7 @@ namespace FdoToolbox.Core.Controls
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(176, 248);
+            this.groupBox1.Size = new System.Drawing.Size(220, 248);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Schemas";
@@ -94,7 +97,7 @@ namespace FdoToolbox.Core.Controls
             this.lstSchemas.FormattingEnabled = true;
             this.lstSchemas.Location = new System.Drawing.Point(3, 41);
             this.lstSchemas.Name = "lstSchemas";
-            this.lstSchemas.Size = new System.Drawing.Size(170, 199);
+            this.lstSchemas.Size = new System.Drawing.Size(214, 199);
             this.lstSchemas.TabIndex = 1;
             this.lstSchemas.SelectedIndexChanged += new System.EventHandler(this.lstSchemas_SelectedIndexChanged);
             // 
@@ -102,10 +105,11 @@ namespace FdoToolbox.Core.Controls
             // 
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.btnAddSchema,
-            this.btnDeleteSchema});
+            this.btnDeleteSchema,
+            this.btnSaveSchema});
             this.toolStrip1.Location = new System.Drawing.Point(3, 16);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(170, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(214, 25);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -127,6 +131,36 @@ namespace FdoToolbox.Core.Controls
             this.btnDeleteSchema.Text = "Delete";
             this.btnDeleteSchema.Click += new System.EventHandler(this.btnDeleteSchema_Click);
             // 
+            // btnSaveSchema
+            // 
+            this.btnSaveSchema.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asXMLToolStripMenuItem,
+            this.asNewSDFToolStripMenuItem});
+            this.btnSaveSchema.Image = global::FdoToolbox.Core.Properties.Resources.disk;
+            this.btnSaveSchema.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveSchema.Name = "btnSaveSchema";
+            this.btnSaveSchema.Size = new System.Drawing.Size(60, 22);
+            this.btnSaveSchema.Text = "Save";
+            this.btnSaveSchema.Visible = false;
+            // 
+            // asXMLToolStripMenuItem
+            // 
+            this.asXMLToolStripMenuItem.Image = global::FdoToolbox.Core.Properties.Resources.page_white_code;
+            this.asXMLToolStripMenuItem.Name = "asXMLToolStripMenuItem";
+            this.asXMLToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.asXMLToolStripMenuItem.Text = "As XML";
+            this.asXMLToolStripMenuItem.ToolTipText = "Save the selected schema definition to an XML file";
+            this.asXMLToolStripMenuItem.Click += new System.EventHandler(this.SaveSchemaAsXML_Click);
+            // 
+            // asNewSDFToolStripMenuItem
+            // 
+            this.asNewSDFToolStripMenuItem.Image = global::FdoToolbox.Core.Properties.Resources.database;
+            this.asNewSDFToolStripMenuItem.Name = "asNewSDFToolStripMenuItem";
+            this.asNewSDFToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.asNewSDFToolStripMenuItem.Text = "As New SDF";
+            this.asNewSDFToolStripMenuItem.ToolTipText = "Apply this selected schema to a new SDF file";
+            this.asNewSDFToolStripMenuItem.Click += new System.EventHandler(this.SaveSchemaAsSDF_Click);
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.lstClasses);
@@ -134,7 +168,7 @@ namespace FdoToolbox.Core.Controls
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(0, 0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(305, 248);
+            this.groupBox2.Size = new System.Drawing.Size(261, 248);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Classes";
@@ -146,7 +180,7 @@ namespace FdoToolbox.Core.Controls
             this.lstClasses.FormattingEnabled = true;
             this.lstClasses.Location = new System.Drawing.Point(3, 41);
             this.lstClasses.Name = "lstClasses";
-            this.lstClasses.Size = new System.Drawing.Size(299, 199);
+            this.lstClasses.Size = new System.Drawing.Size(255, 199);
             this.lstClasses.TabIndex = 1;
             // 
             // toolStrip2
@@ -157,7 +191,7 @@ namespace FdoToolbox.Core.Controls
             this.btnDeleteClass});
             this.toolStrip2.Location = new System.Drawing.Point(3, 16);
             this.toolStrip2.Name = "toolStrip2";
-            this.toolStrip2.Size = new System.Drawing.Size(299, 25);
+            this.toolStrip2.Size = new System.Drawing.Size(255, 25);
             this.toolStrip2.TabIndex = 0;
             this.toolStrip2.Text = "toolStrip2";
             // 
@@ -298,5 +332,8 @@ namespace FdoToolbox.Core.Controls
         private System.Windows.Forms.ToolStripMenuItem networkLayerClassToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem networkLinkClassToolStripMenuItem;
         private System.Windows.Forms.ToolStripButton btnDeleteClass;
+        private System.Windows.Forms.ToolStripDropDownButton btnSaveSchema;
+        private System.Windows.Forms.ToolStripMenuItem asXMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asNewSDFToolStripMenuItem;
     }
 }
