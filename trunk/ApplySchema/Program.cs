cc8b77ea9488baa2630e5050b767ce5c66257103
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-07-27 00:53:38 +0000

    Issue 60: Command-line utilities
    Implement ApplySchema.exe

---
+++ trunk/ApplySchema/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplySchema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ApplySchemaApp().Run(args);
+        }
+    }
+}
