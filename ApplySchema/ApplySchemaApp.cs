0b28f33fc09883433ae532854b87fde307e6ed0c
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-07-27 00:53:38 +0000

    Issue 60: Command-line utilities
    Implement ApplySchema.exe

---
+++ ApplySchema/ApplySchemaApp.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FdoToolbox.Core;
+using OSGeo.FDO.Connections;
+using OSGeo.FDO.ClientServices;
+using System.IO;
+using OSGeo.FDO.Schema;
+using OSGeo.FDO.Commands.Schema;
+
+namespace ApplySchema
+{
+    public class ApplySchemaApp : ConsoleApplication
+    {
+        private string _FdoProvider;
+
+        public string FdoProvider
+        {
+            get { return _FdoProvider; }
+            set { _FdoProvider = value; }
+        }
+
+        private string _ConnectionString;
+
+        public string ConnectionString
+        {
+            get { return _ConnectionString; }
+            set { _ConnectionString = value; }
+        }
+
+        private string _FileName;
+
+        public string FileName
+        {
+            get { return _FileName; }
+            set { _FileName = value; }
+        }
+
+        public override void ParseArguments(string[] args, int minArguments, int maxArguments)
+        {
+            string file = GetArgument("-file", args);
+            string provider = GetArgument("-provider", args);
+            string connStr = GetArgument("-connection", args);
+
+            if (string.IsNullOrEmpty(file))
+                throw new ArgumentException("-file parameter required");
+            else
+                this.FileName = CheckFile(file);
+
+            if (string.IsNullOrEmpty(provider))
+                throw new ArgumentException("-provider parameter required");
+            else
+                this.FdoProvider = provider;
+
+            if (string.IsNullOrEmpty(connStr))
+                throw new ArgumentException("-connection parameter required");
+            else
+                this.ConnectionString = connStr;
+        }
+
+        public override void ShowUsage()
+        {
+            AppConsole.WriteLine("Usage: ApplySchema.exe -file:<schema definition file> -provider:<provider> -connection:<connection string>");
+        }
+
+        public override void Run(string[] args)
+        {
+            try
+            {
+                ParseArguments(args, 3, 3);
+            }
+            catch (ArgumentException ex)
+            {
+                AppConsole.WriteLine(ex.Message);
+                ShowUsage();
+                return;
+            }
+
+            try
+            {
+                IConnection conn = FeatureAccessManager.GetConnectionManager().CreateConnection(this.FdoProvider);
+                conn.ConnectionString = this.ConnectionString;
+                conn.Open();
+                using (conn)
+                {
+                    using (FeatureSchemaCollection schemas = new FeatureSchemaCollection(null))
+                    {
+                        schemas.ReadXml(this.FileName);
+                        using (IApplySchema apply = conn.CreateCommand(OSGeo.FDO.Commands.CommandType.CommandType_ApplySchema) as IApplySchema)
+                        {
+                            apply.FeatureSchema = schemas[0];
+                            apply.Execute();
+                            AppConsole.WriteLine("Schema applied!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                AppConsole.WriteException(ex);
+            }
+        }
+    }
+}
