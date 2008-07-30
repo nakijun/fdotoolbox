ed4540f4858a3986880f5ef6356940607c2a2392
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-07-30 07:04:44 +0000

    Add FdoInfo project

---
+++ FdoInfo/FdoInfoApp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FdoToolbox.Core;
+
+namespace FdoInfo
+{
+    public class FdoInfoApp : ConsoleApplication
+    {
+        public override void ParseArguments(string[] args)
+        {
+            
+        }
+
+        public override void ShowUsage()
+        {
+            
+        }
+
+        public override void Run(string[] args)
+        {
+            
+        }
+    }
+}
