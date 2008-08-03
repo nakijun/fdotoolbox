a961626d3512da5d0a0ead69a8397750d53fbd81
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-08-03 00:33:33 +0000

    clean script update

--- clean.bat
+++ clean.bat
@@ -1,6 +1,15 @@
 @echo off
 echo "Cleaning Output Directory"
 rd /S /Q out
+echo "Cleaning FdoInfo"
+rd /S /Q FdoInfo\bin
+rd /S /Q FdoInfo\obj
+echo "Cleaning FdoUtil"
+rd /S /Q FdoUtil\bin
+rd /S /Q FdoUtil\obj
+echo "Cleaning TestApp"
+rd /S /Q TestApp\bin
+rd /S /Q TestApp\obj
 echo "Cleaning FdoToolbox"
 rd /S /Q FdoToolbox\bin
 rd /S /Q FdoToolbox\obj
