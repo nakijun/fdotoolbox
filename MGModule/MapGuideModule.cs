22165c7acbe48e71bb85cc8cb5a4f8f0110cf62b
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-09-02 09:36:04 +0000

    Issue 69: Add MGModule project

---
+++ MGModule/MapGuideModule.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FdoToolbox.Core.Modules;
+
+namespace MGModule
+{
+    public class MapGuideModule : ModuleBase
+    {
+        public override string Name
+        {
+            get { return "mapguide"; }
+        }
+
+        public override string Description
+        {
+            get { return "MapGuide Extension Module"; }
+        }
+
+        public override void Initialize()
+        {
+            
+        }
+
+        public override void Cleanup()
+        {
+            
+        }
+    }
+}
