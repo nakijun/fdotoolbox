205825588280bc2f07e001bf9e07ccd23f232ced
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2008-09-02 11:54:25 +0000

    Make ObjectExplorer use the new APIs

--- FdoToolbox/ObjectExplorer.Designer.cs
+++ FdoToolbox/ObjectExplorer.Designer.cs
@@ -29,15 +29,7 @@ namespace FdoToolbox
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("FDO Data Sources", 0, 0);
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("ADO.net Data Sources");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Tasks", 1, 1);
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Modules", 2, 2);
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ObjectExplorer));
-            this.ctxFdoConnections = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.ctxDbConnections = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.ctxTasks = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.ctxModules = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.mToolStrip = new System.Windows.Forms.ToolStrip();
             this.mTreeView = new System.Windows.Forms.TreeView();
             this.mImageList = new System.Windows.Forms.ImageList(this.components);
@@ -50,26 +42,6 @@ namespace FdoToolbox
             this.ctxSelectedTable = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.ctxSelectedDatabase = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
-            // 
-            // ctxFdoConnections
-            // 
-            this.ctxFdoConnections.Name = "ctxConnections";
-            this.ctxFdoConnections.Size = new System.Drawing.Size(61, 4);
-            // 
-            // ctxDbConnections
-            // 
-            this.ctxDbConnections.Name = "ctxSelectedDatabase";
-            this.ctxDbConnections.Size = new System.Drawing.Size(61, 4);
-            // 
-            // ctxTasks
-            // 
-            this.ctxTasks.Name = "ctxTasks";
-            this.ctxTasks.Size = new System.Drawing.Size(61, 4);
-            // 
-            // ctxModules
-            // 
-            this.ctxModules.Name = "ctxModules";
-            this.ctxModules.Size = new System.Drawing.Size(61, 4);
             // 
             // mToolStrip
             // 
@@ -86,29 +58,6 @@ namespace FdoToolbox
             this.mTreeView.ImageList = this.mImageList;
             this.mTreeView.Location = new System.Drawing.Point(0, 25);
             this.mTreeView.Name = "mTreeView";
-            treeNode1.ContextMenuStrip = this.ctxFdoConnections;
-            treeNode1.ImageIndex = 0;
-            treeNode1.Name = "NODE_FDO_CONNECTIONS";
-            treeNode1.SelectedImageIndex = 0;
-            treeNode1.Text = "FDO Data Sources";
-            treeNode2.ContextMenuStrip = this.ctxDbConnections;
-            treeNode2.Name = "NODE_DB_CONNECTIONS";
-            treeNode2.Text = "ADO.net Data Sources";
-            treeNode3.ContextMenuStrip = this.ctxTasks;
-            treeNode3.ImageIndex = 1;
-            treeNode3.Name = "NODE_TASKS";
-            treeNode3.SelectedImageIndex = 1;
-            treeNode3.Text = "Tasks";
-            treeNode4.ContextMenuStrip = this.ctxModules;
-            treeNode4.ImageIndex = 2;
-            treeNode4.Name = "NODE_MODULES";
-            treeNode4.SelectedImageIndex = 2;
-            treeNode4.Text = "Modules";
-            this.mTreeView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3,
-            treeNode4});
             this.mTreeView.SelectedImageIndex = 0;
             this.mTreeView.ShowNodeToolTips = true;
             this.mTreeView.Size = new System.Drawing.Size(292, 241);
@@ -197,14 +146,10 @@ namespace FdoToolbox
         private System.Windows.Forms.ContextMenuStrip ctxSelectedModule;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedTask;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedSpatialConnection;
-        private System.Windows.Forms.ContextMenuStrip ctxFdoConnections;
-        private System.Windows.Forms.ContextMenuStrip ctxTasks;
-        private System.Windows.Forms.ContextMenuStrip ctxModules;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedSchema;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedClass;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedDatabaseConnection;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedTable;
-        private System.Windows.Forms.ContextMenuStrip ctxDbConnections;
         private System.Windows.Forms.ContextMenuStrip ctxSelectedDatabase;
     }
 }
