14e9dc1224175c016d9a0765de4ecaeddbc8237a
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2009-01-01 07:42:02 +0000

    This submissions adds missing public API documentation.

--- FdoToolbox.Core/AppFramework/ConsoleColorSwitcher.cs
+++ FdoToolbox.Core/AppFramework/ConsoleColorSwitcher.cs
@@ -4,16 +4,28 @@ using System.Text;
 
 namespace FdoToolbox.Core.AppFramework
 {
+    /// <summary>
+    /// A helper object to switch the text color of the console. All text written
+    /// out to the console will be of the specified color until this object is 
+    /// disposed of.
+    /// </summary>
     public class ConsoleColorSwitcher : IDisposable
     {
         private ConsoleColor originalColor;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConsoleColorSwitcher"/> class.
+        /// </summary>
+        /// <param name="color">The color.</param>
         public ConsoleColorSwitcher(ConsoleColor color)
         {
             originalColor = Console.ForegroundColor;
             Console.ForegroundColor = color;
         }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             Console.ForegroundColor = originalColor;
