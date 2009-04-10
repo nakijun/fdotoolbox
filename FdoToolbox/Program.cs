1fdc46e4bf4c83e8f9e2b4b9897f7c124b1730ab
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2009-04-10 15:53:12 +0000

    Fix Issue 177: Make app single-instance using the technique described here: http://sanity-free.org/143/csharp_dotnet_single_instance_application.html

--- FdoToolbox/Program.cs
+++ FdoToolbox/Program.cs
@@ -29,109 +29,124 @@ using System.IO;
 using System.Resources;
 using FdoToolbox.Base.Services;
 using FdoToolbox.Core;
+using System.Threading;
 
 namespace FdoToolbox
 {
     static class Program
     {
+        static Mutex appMutex = new Mutex(true, "{E6C2B9BD-6614-409a-9845-BBB23C9539B7}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(true);
-            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
-
-            // The LoggingService is a small wrapper around log4net.
-            // Our application contains a .config file telling log4net to write
-            // to System.Diagnostics.Trace.
-            LoggingService.Info("Application start");
-
-            AppDomain.CurrentDomain.AssemblyLoad += new AssemblyLoadEventHandler(CurrentDomain_AssemblyLoad);
-
-            // Get a reference to the entry assembly (Startup.exe)
-            Assembly exe = typeof(Program).Assembly;
-
-            // Set the root path of our application. ICSharpCode.Core looks for some other
-            // paths relative to the application root:
-            // "data/resources" for language resources, "data/options" for default options
-            FileUtility.ApplicationRootPath = Path.GetDirectoryName(exe.Location);
-
-            LoggingService.Info("Starting core services...");
-
-            // CoreStartup is a helper class making starting the Core easier.
-            // The parameter is used as the application name, e.g. for the default title of
-            // MessageService.ShowMessage() calls.
-            CoreStartup coreStartup = new CoreStartup("FDO Toolbox");
-            // It is also used as default storage location for the application settings:
-            // "%Application Data%\%Application Name%", but you can override that by setting c.ConfigDirectory
-
-            // Specify the name of the application settings file (.xml is automatically appended)
-            coreStartup.PropertiesName = "AppProperties";
-
-            // Initializes the Core services (ResourceService, PropertyService, etc.)
-            coreStartup.StartCoreServices();
-
-            LoggingService.Info("Looking for AddIns...");
-            // Searches for ".addin" files in the application directory.
-            coreStartup.AddAddInsFromDirectory(Path.Combine(FileUtility.ApplicationRootPath, "AddIns"));
-
-            // Searches for a "AddIns.xml" in the user profile that specifies the names of the
-            // add-ins that were deactivated by the user, and adds "external" AddIns.
-            coreStartup.ConfigureExternalAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddIns.xml"));
-
-            // Searches for add-ins installed by the user into his profile directory. This also
-            // performs the job of installing, uninstalling or upgrading add-ins if the user
-            // requested it the last time this application was running.
-            coreStartup.ConfigureUserAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddInInstallTemp"),
-                                            Path.Combine(PropertyService.ConfigDirectory, "AddIns"));
-
-            LoggingService.Info("Checking FDO");
-            // Set FDO path
-            string fdoPath = Preferences.FdoPath;
-            if (!FdoAssemblyResolver.IsValidFdoPath(fdoPath))
+            if (appMutex.WaitOne(TimeSpan.Zero, true))
             {
-                fdoPath = Path.Combine(FileUtility.ApplicationRootPath, "FDO");
-                Preferences.FdoPath = fdoPath;
-            }
-            AddInAssemblyResolver.RegisterLibraries(fdoPath, "OSGeo.FDO.dll", "OSGeo.FDO.Common.dll", "OSGeo.FDO.Geometry.dll", "OSGeo.FDO.Spatial.dll");
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(true);
+                Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
 
-            LoggingService.Info("FDO path set to: " + fdoPath);
+                // The LoggingService is a small wrapper around log4net.
+                // Our application contains a .config file telling log4net to write
+                // to System.Diagnostics.Trace.
+                LoggingService.Info("Application start");
 
-            LoggingService.Info("Loading AddInTree...");
-            // Now finally initialize the application. This parses the ".addin" files and
-            // creates the AddIn tree. It also automatically runs the commands in
-            // "/Workspace/Autostart"
-            coreStartup.RunInitialization();
+                AppDomain.CurrentDomain.AssemblyLoad += new AssemblyLoadEventHandler(CurrentDomain_AssemblyLoad);
 
-            LoggingService.Info("Initializing Workbench...");
-            // Workbench is our class from the base project, this method creates an instance
-            // of the main form.
-            log4net.Config.XmlConfigurator.Configure();
-            Workbench.InitializeWorkbench();
-            
-            try
-            {
-                LoggingService.Info("Running application...");
-                // Workbench.Instance is the instance of the main form, run the message loop.
-                Application.Run(Workbench.Instance);
-            }
-            finally
-            {
+                // Get a reference to the entry assembly (Startup.exe)
+                Assembly exe = typeof(Program).Assembly;
+
+                // Set the root path of our application. ICSharpCode.Core looks for some other
+                // paths relative to the application root:
+                // "data/resources" for language resources, "data/options" for default options
+                FileUtility.ApplicationRootPath = Path.GetDirectoryName(exe.Location);
+
+                LoggingService.Info("Starting core services...");
+
+                // CoreStartup is a helper class making starting the Core easier.
+                // The parameter is used as the application name, e.g. for the default title of
+                // MessageService.ShowMessage() calls.
+                CoreStartup coreStartup = new CoreStartup("FDO Toolbox");
+                // It is also used as default storage location for the application settings:
+                // "%Application Data%\%Application Name%", but you can override that by setting c.ConfigDirectory
+
+                // Specify the name of the application settings file (.xml is automatically appended)
+                coreStartup.PropertiesName = "AppProperties";
+
+                // Initializes the Core services (ResourceService, PropertyService, etc.)
+                coreStartup.StartCoreServices();
+
+                LoggingService.Info("Looking for AddIns...");
+                // Searches for ".addin" files in the application directory.
+                coreStartup.AddAddInsFromDirectory(Path.Combine(FileUtility.ApplicationRootPath, "AddIns"));
+
+                // Searches for a "AddIns.xml" in the user profile that specifies the names of the
+                // add-ins that were deactivated by the user, and adds "external" AddIns.
+                coreStartup.ConfigureExternalAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddIns.xml"));
+
+                // Searches for add-ins installed by the user into his profile directory. This also
+                // performs the job of installing, uninstalling or upgrading add-ins if the user
+                // requested it the last time this application was running.
+                coreStartup.ConfigureUserAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddInInstallTemp"),
+                                                Path.Combine(PropertyService.ConfigDirectory, "AddIns"));
+
+                LoggingService.Info("Checking FDO");
+                // Set FDO path
+                string fdoPath = Preferences.FdoPath;
+                if (!FdoAssemblyResolver.IsValidFdoPath(fdoPath))
+                {
+                    fdoPath = Path.Combine(FileUtility.ApplicationRootPath, "FDO");
+                    Preferences.FdoPath = fdoPath;
+                }
+                AddInAssemblyResolver.RegisterLibraries(fdoPath, "OSGeo.FDO.dll", "OSGeo.FDO.Common.dll", "OSGeo.FDO.Geometry.dll", "OSGeo.FDO.Spatial.dll");
+
+                LoggingService.Info("FDO path set to: " + fdoPath);
+
+                LoggingService.Info("Loading AddInTree...");
+                // Now finally initialize the application. This parses the ".addin" files and
+                // creates the AddIn tree. It also automatically runs the commands in
+                // "/Workspace/Autostart"
+                coreStartup.RunInitialization();
+
+                LoggingService.Info("Initializing Workbench...");
+                // Workbench is our class from the base project, this method creates an instance
+                // of the main form.
+                log4net.Config.XmlConfigurator.Configure();
+                Workbench.InitializeWorkbench();
+
                 try
                 {
-                    // Save changed properties
-                    PropertyService.Save();
+                    LoggingService.Info("Running application...");
+                    // Workbench.Instance is the instance of the main form, run the message loop.
+                    Application.Run(Workbench.Instance);
                 }
-                catch (Exception ex)
+                finally
                 {
-                    MessageService.ShowError(ex, "Error storing properties");
+                    try
+                    {
+                        // Save changed properties
+                        PropertyService.Save();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageService.ShowError(ex, "Error storing properties");
+                    }
                 }
+                LoggingService.Info("Application shutdown");
+                appMutex.ReleaseMutex();
             }
-
-            LoggingService.Info("Application shutdown");
+            else
+            {
+                //Send our message to make the workbench be the topmost form
+                NativeMethods.PostMessage(
+                    (IntPtr)NativeMethods.HWND_BROADCAST,
+                    NativeMethods.WM_SHOWME,
+                    IntPtr.Zero,
+                    IntPtr.Zero);
+            }
         }
 
         static void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
