707fa289a12ed56398afc8cf41c650b6036cf8ac
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2009-04-13 08:52:38 +0000

    Issue 125, Issue 126: More work on the Object and Association property editors. Because this functionality is not ready for prime-time yet, this functionality is wrapped around an EXTENDED_PROPERTY_SUPPORT preprocessor variable, that will be turned off for future releases until it is ready to go.

--- FdoToolbox.Base/Controls/SchemaDesigner/ObjectPropertyDefinitionDesign.cs
+++ FdoToolbox.Base/Controls/SchemaDesigner/ObjectPropertyDefinitionDesign.cs
@@ -24,27 +24,37 @@ using System.Collections.Generic;
 using System.Text;
 using FdoToolbox.Core.Feature;
 using System.ComponentModel;
+using OSGeo.FDO.Schema;
+using System.Windows.Forms.Design;
+using System.Drawing.Design;
+using System.Windows.Forms;
 
 namespace FdoToolbox.Base.Controls.SchemaDesigner
 {
     public class ObjectPropertyDefinitionDesign : PropertyDefinitionDesign
     {
-        private OSGeo.FDO.Schema.ObjectPropertyDefinition _objDef;
+        private ObjectPropertyDefinition _objDef;
         
-        public ObjectPropertyDefinitionDesign(OSGeo.FDO.Schema.ObjectPropertyDefinition op)
+        public ObjectPropertyDefinitionDesign(ObjectPropertyDefinition op)
             : base(op)
         {
             _objDef = op;
         }
 
-        public ObjectPropertyDefinitionDesign(OSGeo.FDO.Schema.ObjectPropertyDefinition op, FdoConnection conn)
+        public ObjectPropertyDefinitionDesign(ObjectPropertyDefinition op, FdoConnection conn)
             : base(op, conn)
         {
             _objDef = op;
         }
 
+        internal ClassDefinition GetParent()
+        {
+            return _objDef.Parent as ClassDefinition;
+        }
+
         [Description("The reference to the class definition that defines the type of this property")]
-        public OSGeo.FDO.Schema.ClassDefinition Class
+        [Editor(typeof(ObjectClassBrowserEditor), typeof(UITypeEditor))]
+        public ClassDefinition Class
         {
             get { return _objDef.Class; }
             set
@@ -55,7 +65,8 @@ namespace FdoToolbox.Base.Controls.SchemaDesigner
         }
 
         [Description("The referecnes to a Data Property Definition to use for uniquely identifying instances of the contained class within a single parent object instance. This value is only used for the ObjectType_Collection and ObjectType_OrderedCollection object property types. The Data Property Definition must belong to the Class Definition that defines the type of this property.")]
-        public OSGeo.FDO.Schema.DataPropertyDefinition IdentityProperty
+        [Editor(typeof(ObjectClassPropertyBrowserEditor), typeof(UITypeEditor))]
+        public DataPropertyDefinition IdentityProperty
         {
             get { return _objDef.IdentityProperty; }
             set
@@ -66,7 +77,7 @@ namespace FdoToolbox.Base.Controls.SchemaDesigner
         }
 
         [Description("The type of this object property (value, collection or ordered collection)")]
-        public OSGeo.FDO.Schema.ObjectType ObjectType
+        public ObjectType ObjectType
         {
             get { return _objDef.ObjectType; }
             set
@@ -77,7 +88,7 @@ namespace FdoToolbox.Base.Controls.SchemaDesigner
         }
 
         [Description("The order type of this object property (ascending or descending). This property is only applicable if the property type is set to ObjectType_OrderedCollection")]
-        public OSGeo.FDO.Schema.OrderType OrderType
+        public OrderType OrderType
         {
             get { return _objDef.OrderType; }
             set
@@ -85,6 +96,113 @@ namespace FdoToolbox.Base.Controls.SchemaDesigner
                 _objDef.OrderType = value;
                 this.FirePropertyChanged("OrderType");
             }
+        }
+    }
+
+    internal class ObjectClassBrowserEditor : UITypeEditor
+    {
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (provider != null)
+            {
+                ObjectPropertyDefinitionDesign ad = (ObjectPropertyDefinitionDesign)context.Instance;
+                ClassDefinition cd = ad.GetParent();
+                if (cd != null)
+                {
+                    FeatureSchema schema = cd.Parent as FeatureSchema;
+                    if (schema != null)
+                    {
+                        ListBox lb = new ListBox();
+                        lb.SelectionMode = SelectionMode.One;
+
+                        foreach (ClassDefinition cls in schema.Classes)
+                        {
+                            if (cls.Name != cd.Name)
+                                lb.Items.Add(cls.Name);
+                        }
+
+                        IWindowsFormsEditorService editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+
+                        lb.SelectedIndexChanged += delegate
+                        {
+                            if (lb.SelectedItem != null)
+                                editorService.CloseDropDown();
+                        };
+
+                        editorService.DropDownControl(lb);
+
+                        if (lb.SelectedItem != null)
+                        {
+                            int idx = schema.Classes.IndexOf(lb.SelectedItem.ToString());
+                            if (idx >= 0)
+                                value = schema.Classes[idx];
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.DropDown;
+        }
+    }
+
+    internal class ObjectClassPropertyBrowserEditor : UITypeEditor
+    {
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (provider != null)
+            {
+                ObjectPropertyDefinitionDesign od = (ObjectPropertyDefinitionDesign)context.Instance;
+                DataPropertyDefinitionCollection dpcol = (DataPropertyDefinitionCollection)value;
+                ClassDefinition cd = od.Class;
+                if (cd != null)
+                {
+                    CheckedListBox box = new CheckedListBox();
+
+                    foreach (PropertyDefinition pd in cd.Properties)
+                    {
+                        DataPropertyDefinition dp = pd as DataPropertyDefinition;
+                        if (dp != null)
+                            box.Items.Add(dp.Name, dpcol.Contains(dp));
+                    }
+
+                    IWindowsFormsEditorService editorService = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+                    editorService.DropDownControl(box);
+
+                    if (box.CheckedItems.Count > 0)
+                    {
+                        DataPropertyDefinitionCollection dpdc = new DataPropertyDefinitionCollection(cd);
+                        foreach (object obj in box.CheckedItems)
+                        {
+                            int pidx = cd.Properties.IndexOf(obj.ToString());
+                            if (pidx >= 0)
+                            {
+                                PropertyDefinition pd = cd.Properties[pidx];
+                                if (pd.PropertyType == PropertyType.PropertyType_DataProperty)
+                                    dpdc.Add((DataPropertyDefinition)pd);
+                            }
+                        }
+                        value = dpdc;
+                    }
+                    else
+                    {
+                        dpcol.Clear();
+                    }
+                }
+                else
+                {
+                    ICSharpCode.Core.MessageService.ShowError("No referenced class specified");
+                }
+            }
+            return value;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.DropDown;
         }
     }
 }
