0bda4120fadbc97f8d82ce5ad7184819f43f9a5a
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2009-06-09 17:01:05 +0000

    Set 0.7.8 release build number

--- branches/0.7.8/Properties/GlobalAssemblyInfo.cs
+++ branches/0.7.8/Properties/GlobalAssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 
 //when making releases, change the * to the SVN revision number
 //of the WC used to produce the release.
-[assembly: AssemblyVersion("0.7.8.*")]
+[assembly: AssemblyVersion("0.7.8.912")]
