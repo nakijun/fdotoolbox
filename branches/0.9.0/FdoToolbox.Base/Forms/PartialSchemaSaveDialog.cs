c63322167d8a34d5e0bc5f768823d2a147f1e8fd
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2009-11-10 12:09:33 +0000

    Create 0.9.0 branch

---
+++ branches/0.9.0/FdoToolbox.Base/Forms/PartialSchemaSaveDialog.cs
@@ -0,0 +1,320 @@
+#region LGPL Header
+// Copyright (C) 2009, Jackie Ng
+// http://code.google.com/p/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OSGeo.FDO.Schema;
+using FdoToolbox.Core.Feature;
+using FdoToolbox.Core.Utility;
+using ICSharpCode.Core;
+using FdoToolbox.Base.Services;
+
+namespace FdoToolbox.Base.Forms
+{
+    public partial class PartialSchemaSaveDialog : Form
+    {
+        const int IDX_CLASS = 0;
+        const int IDX_FEATURECLASS = 1;
+        const int IDX_KEY = 2;
+        const int IDX_DATA = 3;
+        const int IDX_GEOMETRY = 4;
+        const int IDX_OBJECT = 5;
+        const int IDX_ASSOCIATION = 6;
+        const int IDX_RASTER = 7;
+
+        private PartialSchemaSaveDialog()
+        {
+            InitializeComponent();
+        }
+
+        private FeatureSchema _schema;
+
+        public PartialSchemaSaveDialog(FeatureSchema schema)
+            : this()
+        {
+            _schema = schema;
+            InitTree();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            btnSave.Enabled = CanSave();
+            base.OnLoad(e);
+        }
+
+        private void InitTree()
+        {
+            //Pre-sort
+            SortedList<string, ClassDefinition> sorted = new SortedList<string, ClassDefinition>();
+            foreach (ClassDefinition cls in _schema.Classes)
+                sorted.Add(cls.Name, cls);
+
+            foreach (ClassDefinition cls in sorted.Values)
+            {
+                TreeNode clsNode = new TreeNode();
+                clsNode.Name = cls.Name;
+                clsNode.Text = cls.Name;
+                clsNode.Checked = true;
+
+                if (cls.ClassType == ClassType.ClassType_FeatureClass)
+                    clsNode.ImageIndex = clsNode.SelectedImageIndex = IDX_FEATURECLASS;
+                else
+                    clsNode.ImageIndex = clsNode.SelectedImageIndex = IDX_CLASS;
+
+                //Pre-sort
+                SortedList<string, PropertyDefinition> psorted = new SortedList<string, PropertyDefinition>();
+                foreach (PropertyDefinition pd in cls.Properties)
+                    psorted.Add(pd.Name, pd);
+
+                foreach (PropertyDefinition pd in psorted.Values)
+                {
+                    TreeNode propNode = new TreeNode();
+                    propNode.Name = pd.Name;
+                    propNode.Text = pd.Name;
+                    propNode.Checked = true;
+
+                    switch (pd.PropertyType)
+                    {
+                        case PropertyType.PropertyType_AssociationProperty:
+                            propNode.ImageIndex = propNode.SelectedImageIndex = IDX_ASSOCIATION;
+                            break;
+                        case PropertyType.PropertyType_DataProperty:
+                            if (cls.IdentityProperties.Contains(pd.Name))
+                                propNode.ImageIndex = propNode.SelectedImageIndex = IDX_KEY;
+                            else
+                                propNode.ImageIndex = propNode.SelectedImageIndex = IDX_DATA;
+                            break;
+                        case PropertyType.PropertyType_GeometricProperty:
+                            propNode.ImageIndex = propNode.SelectedImageIndex = IDX_GEOMETRY;
+                            break;
+                        case PropertyType.PropertyType_ObjectProperty:
+                            propNode.ImageIndex = propNode.SelectedImageIndex = IDX_OBJECT;
+                            break;
+                        case PropertyType.PropertyType_RasterProperty:
+                            propNode.ImageIndex = propNode.SelectedImageIndex = IDX_RASTER;
+                            break;
+                    }
+
+                    clsNode.Nodes.Add(propNode);
+                }
+
+                treeSchema.Nodes.Add(clsNode);
+            }
+        }
+
+        private void btnCheckAll_Click(object sender, EventArgs e)
+        {
+            CheckAll(treeSchema.Nodes, true);
+        }
+
+        private static void CheckAll(TreeNodeCollection nodes, bool state)
+        {
+            foreach (TreeNode node in nodes)
+            {
+                node.Checked = state;
+                if (node.Nodes.Count > 0)
+                {
+                    CheckAll(node.Nodes, state);
+                }
+            }
+        }
+
+        private void btnCheckNone_Click(object sender, EventArgs e)
+        {
+            CheckAll(treeSchema.Nodes, false);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //Operate on a clone
+            FeatureSchema schema = FdoFeatureService.CloneSchema(_schema);
+
+            //Remove elements that have been unchecked. 
+            foreach (TreeNode clsNode in treeSchema.Nodes)
+            {
+                string className = clsNode.Name;
+                int index = schema.Classes.IndexOf(className);
+                if (!clsNode.Checked)
+                {
+                    if (index >= 0)
+                        schema.Classes.RemoveAt(index);
+                }
+                else
+                {
+                    if (index >= 0)
+                    {
+                        ClassDefinition clsDef = schema.Classes[index];
+                        foreach (TreeNode propNode in clsNode.Nodes)
+                        {
+                            if (!propNode.Checked)
+                            {
+                                string propName = propNode.Text;
+                                int pidx = clsDef.Properties.IndexOf(propName);
+                                if (pidx >= 0)
+                                {
+                                    clsDef.Properties.RemoveAt(pidx);
+                                    if (clsDef.IdentityProperties.Contains(propName))
+                                    {
+                                        int idpdx = clsDef.IdentityProperties.IndexOf(propName);
+                                        clsDef.IdentityProperties.RemoveAt(idpdx);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (rdXml.Checked)
+            {
+                schema.WriteXml(txtXml.Text);
+                MessageService.ShowMessage("Schema saved to: " + txtXml.Text);
+                this.DialogResult = DialogResult.OK;
+            }
+            else if (rdFile.Checked)
+            {
+                string fileName = txtFile.Text;
+                if (ExpressUtility.CreateFlatFileDataSource(fileName))
+                {
+                    FdoConnection conn = ExpressUtility.CreateFlatFileConnection(fileName);
+                    bool disposeConn = true;
+                    using (FdoFeatureService svc = conn.CreateFeatureService())
+                    {
+                        svc.ApplySchema(schema);
+                        if (MessageService.AskQuestion("Schema saved to: " + txtFile.Text + " connect to it?", "Saved"))
+                        {
+                            FdoConnectionManager mgr = ServiceManager.Instance.GetService<FdoConnectionManager>();
+                            string name = MessageService.ShowInputBox(ResourceService.GetString("TITLE_CONNECTION_NAME"), ResourceService.GetString("PROMPT_ENTER_CONNECTION"), "");
+                            if (name == null)
+                                return;
+
+                            while (name == string.Empty || mgr.NameExists(name))
+                            {
+                                MessageService.ShowError(ResourceService.GetString("ERR_CONNECTION_NAME_EMPTY_OR_EXISTS"));
+                                name = MessageService.ShowInputBox(ResourceService.GetString("TITLE_CONNECTION_NAME"), ResourceService.GetString("PROMPT_ENTER_CONNECTION"), name);
+
+                                if (name == null)
+                                    return;
+                            }
+                            disposeConn = false;
+                            mgr.AddConnection(name, conn);
+                        }
+                    }
+                    if (disposeConn)
+                    {
+                        conn.Close();
+                        conn.Dispose();
+                    }
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        private bool CanSave()
+        {
+            if (rdXml.Checked)
+            {
+                return !string.IsNullOrEmpty(txtXml.Text);
+            }
+            else if (rdFile.Checked)
+            {
+                return !string.IsNullOrEmpty(txtFile.Text);
+            }
+            return false;
+        }
+
+        private void treeSchema_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            //Uncheck all children if unchecking class node
+            if (!e.Node.Checked)
+            {
+                if (e.Node.Level == 0) //Class
+                {
+                    foreach (TreeNode pNode in e.Node.Nodes)
+                    {
+                        pNode.Checked = false;
+                    }
+                }
+            }
+            else 
+            {
+                if (e.Node.Level == 1 && !e.Node.Parent.Checked) //Property
+                {
+                    e.Node.Parent.Checked = true;   
+                }
+            }
+
+            btnSave.Enabled = CanSave();
+        }
+
+        private void btnBrowseXml_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog diag = new SaveFileDialog();
+            diag.Filter = "FDO Feature Schemas (*.schema)|*.schema";
+            diag.FileName = _schema.Name + ".schema";
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                txtXml.Text = diag.FileName;
+            }
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog diag = new SaveFileDialog();
+            diag.Filter = "SDF Files (*.sdf)|*.sdf|SQLite files (*.db;*.sqlite;*.slt)|*.db;*.sqlite;*.slt";
+            diag.FileName = _schema.Name;
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                txtFile.Text = diag.FileName;
+            }
+        }
+
+        private void txtXml_TextChanged(object sender, EventArgs e)
+        {
+            btnSave.Enabled = CanSave();
+        }
+
+        private void txtFile_TextChanged(object sender, EventArgs e)
+        {
+            btnSave.Enabled = CanSave();
+        }
+
+        private void rdXml_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSave.Enabled = CanSave();
+        }
+
+        private void rdFile_CheckedChanged(object sender, EventArgs e)
+        {
+            btnSave.Enabled = CanSave();
+        }
+    }
+}
---
+++ branches/0.9.0/FdoToolbox.Core/Feature/FdoConnection.cs
@@ -0,0 +1,392 @@
+#region LGPL Header
+// Copyright (C) 2009, Jackie Ng
+// http://code.google.com/p/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OSGeo.FDO.Connections;
+using FdoToolbox.Core.Feature;
+using OSGeo.FDO.ClientServices;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+using Res = FdoToolbox.Core.ResourceUtil;
+using FdoToolbox.Core.Connections;
+using OSGeo.FDO.Common.Io;
+
+namespace FdoToolbox.Core.Feature
+{
+    /// <summary>
+    /// FDO Connection wrapper class
+    /// </summary>
+    public class FdoConnection : IDisposable
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="conn"></param>
+        internal FdoConnection(IConnection conn)
+        {
+            this.InternalConnection = conn;
+        }
+
+        private ICapability _caps;
+        
+        /// <summary>
+        /// Gets the capability object for this connection
+        /// </summary>
+        public ICapability Capability
+        {
+            get
+            {
+                if (_caps == null)
+                    _caps = new Capability(this);
+                return _caps;
+            }
+        }
+
+        /// <summary>
+        /// Gets the type of the data store.
+        /// </summary>
+        /// <value>The type of the data store.</value>
+        public ProviderDatastoreType DataStoreType
+        {
+            get
+            {
+                return _Connection.ConnectionInfo.ProviderDatastoreType;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FdoConnection"/> class.
+        /// </summary>
+        /// <param name="provider">The provider name.</param>
+        public FdoConnection(string provider)
+        {
+            this.InternalConnection = FeatureAccessManager.GetConnectionManager().CreateConnection(provider);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FdoConnection"/> class.
+        /// </summary>
+        /// <param name="provider">The provider name.</param>
+        /// <param name="connectionString">The connection string.</param>
+        public FdoConnection(string provider, string connectionString)
+            : this(provider)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Saves this connection to a file
+        /// </summary>
+        /// <param name="file"></param>
+        public void Save(string file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(FdoToolbox.Core.Configuration.Connection));
+            using (XmlTextWriter writer = new XmlTextWriter(file, Encoding.UTF8))
+            {
+                writer.Indentation = 4;
+                writer.Formatting = Formatting.Indented;
+
+                FdoToolbox.Core.Configuration.Connection conn = new FdoToolbox.Core.Configuration.Connection();
+                conn.Provider = this.Provider;
+                conn.ConnectionString = this.ConnectionString;
+
+                serializer.Serialize(writer, conn);
+            }
+        }
+
+        /// <summary>
+        /// Creates an FDO connection from file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static FdoConnection LoadFromFile(string file)
+        {
+            FdoToolbox.Core.Configuration.Connection c = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(FdoToolbox.Core.Configuration.Connection));
+            using (StreamReader reader = new StreamReader(file))
+            {
+                c = (FdoToolbox.Core.Configuration.Connection)serializer.Deserialize(reader);
+            }
+            return new FdoConnection(c.Provider, c.ConnectionString);
+        }
+
+        /// <summary>
+        /// Gets or sets the connection string of the underlying connection
+        /// </summary>
+        public string ConnectionString
+        {
+            get { return this.InternalConnection.ConnectionString; }
+            set 
+            { 
+                this.InternalConnection.ConnectionString = value;
+                if (!string.IsNullOrEmpty(value))
+                {
+                    //HACK: ODBC doesn't want to play nice
+                    if (this.Provider.StartsWith("OSGeo.ODBC"))
+                    {
+                        _safeConnStr = value;
+                        return;
+                    }
+
+                    List<string> safeParams = new List<string>();
+                    string[] parameters = this.ConnectionString.Split(';');
+                    IConnectionPropertyDictionary dict = this.InternalConnection.ConnectionInfo.ConnectionProperties;
+                    foreach (string p in parameters)
+                    {
+                        string[] tokens = p.Split('=');
+                        if (!dict.IsPropertyProtected(tokens[0]))
+                        {
+                            safeParams.Add(p);
+                        }
+                        else
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            for (int i = 0; i < tokens[1].Length; i++)
+                            {
+                                sb.Append("*");
+                            }
+                            safeParams.Add(tokens[0] + "=" + sb.ToString());
+                        }
+                    }
+                    _safeConnStr = string.Join(";", safeParams.ToArray());
+                }
+            }
+        }
+
+        private string _safeConnStr = null;
+
+        /// <summary>
+        /// Gets the connection string with the protected elements obfuscated
+        /// </summary>
+        public string SafeConnectionString
+        {
+            get
+            {
+                return _safeConnStr;
+            }
+        }
+
+        private string _name;
+
+        /// <summary>
+        /// The name of the connection's underlying provider. This does not include the version number. Use the <see cref="ProviderQualified"/>
+        /// property for the full provider name
+        /// </summary>
+        public string Provider
+        {
+            get 
+            {
+                if (_name == null)
+                {
+                    ProviderNameTokens providerName = new ProviderNameTokens(this.InternalConnection.ConnectionInfo.ProviderName);
+                    string [] tokens = providerName.GetNameTokens();
+                    _name = tokens[0] + "." + tokens[1];
+                }
+                return _name;
+            }
+        }
+
+        /// <summary>
+        /// The fully-qualified name of the connection's underlying provider
+        /// </summary>
+        public string ProviderQualified
+        {
+            get 
+            {
+                return this.InternalConnection.ConnectionInfo.ProviderName;
+            }
+        }
+
+        /// <summary>
+        /// Refreshes this connection
+        /// </summary>
+        public void Refresh()
+        {
+            Close();
+            this.InternalConnection.Open();
+        }
+
+        private IConnection _Connection;
+
+        /// <summary>
+        /// The underlying FDO connection
+        /// </summary>
+        internal IConnection InternalConnection
+        {
+            get { return _Connection; }
+            set { _Connection = value; }
+        }
+
+        /// <summary>
+        /// Creates a new feature service bound to this connection
+        /// </summary>
+        /// <returns></returns>
+        public FdoFeatureService CreateFeatureService()
+        {
+            if (this.State == FdoConnectionState.Closed)
+                this.Open();
+
+            return new FdoFeatureService(this.InternalConnection);
+        }
+
+        /// <summary>
+        /// Opens the underlying connection
+        /// </summary>
+        public FdoConnectionState Open()
+        {
+            try
+            {
+                if (this.InternalConnection.ConnectionState != ConnectionState.ConnectionState_Open)
+                    this.InternalConnection.Open();
+            }
+            catch (OSGeo.FDO.Common.Exception ex) { throw new FdoException(ex); }
+            return this.State;
+        }
+
+        /// <summary>
+        /// Closes the underlying connection
+        /// </summary>
+        public void Close()
+        {
+            if (this.InternalConnection.ConnectionState != ConnectionState.ConnectionState_Closed)
+                this.InternalConnection.Close();
+        }
+
+        /// <summary>
+        /// Disposes this connection
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Gets the current connection state
+        /// </summary>
+        public FdoConnectionState State
+        {
+            get
+            {
+                switch (this.InternalConnection.ConnectionState)
+                {
+                    case ConnectionState.ConnectionState_Busy:
+                        return FdoConnectionState.Busy;
+                    case ConnectionState.ConnectionState_Closed:
+                        return FdoConnectionState.Closed;
+                    case ConnectionState.ConnectionState_Open:
+                        return FdoConnectionState.Open;
+                    case ConnectionState.ConnectionState_Pending:
+                        return FdoConnectionState.Pending;
+                }
+                throw new InvalidOperationException(Res.GetString("ERR_CONNECTION_UNKNOWN_STATE"));
+            }
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if(_caps != null)
+                    _caps.Dispose();
+                this.Close();
+                this.InternalConnection.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Gets the connect time property.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        public DictionaryProperty GetConnectTimeProperty(string name)
+        {
+            if (this.State != FdoConnectionState.Open && this.State != FdoConnectionState.Pending)
+                throw new InvalidOperationException(Res.GetString("ERR_CONNECTION_NOT_OPEN"));
+
+            IConnectionPropertyDictionary dict = this.InternalConnection.ConnectionInfo.ConnectionProperties;
+            bool enumerable = dict.IsPropertyEnumerable(name);
+            DictionaryProperty dp = null;
+            if (enumerable)
+            {
+                EnumerableDictionaryProperty ep = new EnumerableDictionaryProperty();
+                ep.Values = dict.EnumeratePropertyValues(name);
+                dp = ep;
+            }
+            else
+            {
+                dp = new DictionaryProperty();
+            }
+
+            dp.Name = name;
+            dp.LocalizedName = dict.GetLocalizedName(name);
+            dp.DefaultValue = dict.GetPropertyDefault(name);
+            dp.Protected = dict.IsPropertyProtected(name);
+            dp.Required = dict.IsPropertyRequired(name);
+
+            return dp;
+        }
+
+        /// <summary>
+        /// Sets the configuration for this connection
+        /// </summary>
+        /// <param name="file">The configuration file</param>
+        public void SetConfiguration(string file)
+        {
+            CapabilityType cap = CapabilityType.FdoCapabilityType_SupportsConfiguration;
+            if (!this.Capability.GetBooleanCapability(cap))
+                throw new InvalidOperationException(ResourceUtil.GetStringFormatted("ERR_UNSUPPORTED_CAPABILITY", cap));
+            IoFileStream confStream = new IoFileStream(file, "r");
+            _Connection.Configuration = confStream;
+        }
+    }
+
+    /// <summary>
+    /// Indicates the current connection state
+    /// </summary>
+    public enum FdoConnectionState
+    {
+        /// <summary>
+        /// Connection is busy
+        /// </summary>
+        Busy,
+        /// <summary>
+        /// Connection is open
+        /// </summary>
+        Open,
+        /// <summary>
+        /// Connection is closed
+        /// </summary>
+        Closed,
+        /// <summary>
+        /// Connection is pending. Additional parameters are required in order for it to be open
+        /// </summary>
+        Pending
+    }
+}
