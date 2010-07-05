c567867897f5f5f8aa9db3d167876c0aad4bedce
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2010-07-05 07:47:54 +0000

    Prep 0.9.4 release

--- trunk/Properties/GlobalAssemblyInfo.cs
+++ trunk/Properties/GlobalAssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 
 //when making releases, change the * to the SVN revision number
 //of the WC used to produce the release.
-[assembly: AssemblyVersion("0.9.3.1063")]
+[assembly: AssemblyVersion("0.9.4.1076")]
