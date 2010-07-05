53b94919aa85c978a65aab38cecb4efda040e57b
jumpinjackie <jumpinjackie@7c7806fa-cd4d-0410-ad5f-a31bcf62195d>
2010-07-05 07:49:07 +0000

    Tag 0.9.4 release

---
+++ tags/0.9.4/FdoToolbox.Core/ETL/DefinitionLoader.cs
@@ -0,0 +1,335 @@
+#region LGPL Header
+// Copyright (C) 2009, Jackie Ng
+// http://code.google.com/p/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FdoToolbox.Core.ETL.Specialized;
+using FdoToolbox.Core.Configuration;
+using FdoToolbox.Core.Feature;
+using System.Xml.Serialization;
+using System.IO;
+using OSGeo.FDO.Filter;
+using System.Xml;
+using System.Collections.Specialized;
+
+namespace FdoToolbox.Core.ETL
+{
+    /// <summary>
+    /// Task definition loader base class
+    /// </summary>
+    public abstract class BaseDefinitionLoader
+    {
+        /// <summary>
+        /// Creates the connection.
+        /// </summary>
+        /// <param name="provider">The provider.</param>
+        /// <param name="connStr">The connection string.</param>
+        /// <param name="name">The name that will be assigned to the connection.</param>
+        /// <returns></returns>
+        protected abstract FdoConnection CreateConnection(string provider, string connStr, ref string name);
+
+        /// <summary>
+        /// Prepares the specified bulk copy definition (freshly deserialized) before the loading process begins
+        /// </summary>
+        /// <param name="def">The bulk copy definition.</param>
+        protected abstract void Prepare(FdoBulkCopyTaskDefinition def);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseDefinitionLoader"/> class.
+        /// </summary>
+        protected BaseDefinitionLoader() { }
+
+        /// <summary>
+        /// Loads bulk copy options from xml
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="owner">if set to <c>true</c> [owner].</param>
+        /// <returns></returns>
+        public FdoBulkCopyOptions BulkCopyFromXml(string file, ref string name, bool owner)
+        {
+            FdoBulkCopyTaskDefinition def = null;
+            XmlSerializer ser = new XmlSerializer(typeof(FdoBulkCopyTaskDefinition));
+            def = (FdoBulkCopyTaskDefinition)ser.Deserialize(new StreamReader(file));
+            
+            return BulkCopyFromXml(def, ref name, owner);
+        }
+
+        /// <summary>
+        /// Loads bulk copy options from deserialized xml
+        /// </summary>
+        /// <param name="def">The deserialized definition.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="owner">if set to <c>true</c> [owner].</param>
+        /// <returns></returns>
+        public FdoBulkCopyOptions BulkCopyFromXml(FdoBulkCopyTaskDefinition def, ref string name, bool owner)
+        {
+            Prepare(def);
+
+            name = def.name;
+            Dictionary<string, FdoConnection> connections = new Dictionary<string, FdoConnection>();
+            Dictionary<string, string> changeConnNames = new Dictionary<string, string>();
+
+            foreach (FdoConnectionEntryElement entry in def.Connections)
+            {
+                string connName = entry.name;
+                FdoConnection conn = CreateConnection(entry.provider, entry.ConnectionString, ref connName);
+                connections[connName] = conn;
+
+                if (connName != entry.name)
+                {
+                    changeConnNames[entry.name] = connName;
+                }
+            }
+            
+            foreach (string oldName in changeConnNames.Keys)
+            {
+                def.UpdateConnectionReferences(oldName, changeConnNames[oldName]);
+            }
+
+            FdoBulkCopyOptions opts = new FdoBulkCopyOptions(connections, owner);
+            foreach (FdoCopyTaskElement task in def.CopyTasks)
+            {
+                FdoClassCopyOptions copt = FdoClassCopyOptions.FromElement(task, def.Connections);
+                opts.AddClassCopyOption(copt);
+            }
+            return opts;
+        }
+
+        /// <summary>
+        /// Loads join options from xml
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="owner">if set to <c>true</c> [owner].</param>
+        /// <returns></returns>
+        public FdoJoinOptions JoinFromXml(string file, ref string name, bool owner)
+        {
+            FdoJoinTaskDefinition def = null;
+            XmlSerializer ser = new XmlSerializer(typeof(FdoJoinTaskDefinition));
+            def = (FdoJoinTaskDefinition)ser.Deserialize(new StreamReader(file));
+
+            return JoinFromXml(def, ref name, owner);
+        }
+
+        /// <summary>
+        /// Loads join options from xml
+        /// </summary>
+        /// <param name="def">The deserialized definition</param>
+        /// <param name="name">The name.</param>
+        /// <param name="owner">if set to <c>true</c> [owner].</param>
+        /// <returns></returns>
+        private FdoJoinOptions JoinFromXml(FdoJoinTaskDefinition def, ref string name, bool owner)
+        {
+            FdoJoinOptions opts = new FdoJoinOptions(owner);
+            name = def.name;
+            if (def.JoinSettings.DesignatedGeometry != null)
+            {
+                opts.GeometryProperty = def.JoinSettings.DesignatedGeometry.Property;
+                opts.Side = def.JoinSettings.DesignatedGeometry.Side;
+            }
+            foreach (JoinKey key in def.JoinSettings.JoinKeys)
+            {
+                opts.JoinPairs.Add(key.left, key.right);
+            }
+            string dummy = string.Empty;
+            opts.JoinType = (FdoJoinType)Enum.Parse(typeof(FdoJoinType), def.JoinSettings.JoinType.ToString());
+            opts.SetLeft(
+                CreateConnection(def.Left.Provider, def.Left.ConnectionString, ref dummy),
+                def.Left.FeatureSchema,
+                def.Left.Class);
+            foreach (string p in def.Left.PropertyList)
+            {
+                opts.AddLeftProperty(p);
+            }
+            opts.SetRight(
+                CreateConnection(def.Right.Provider, def.Right.ConnectionString, ref dummy),
+                def.Right.FeatureSchema,
+                def.Right.Class);
+            foreach (string p in def.Right.PropertyList)
+            {
+                opts.AddRightProperty(p);
+            }
+
+            opts.SetTarget(
+                CreateConnection(def.Target.Provider, def.Target.ConnectionString, ref dummy),
+                def.Target.FeatureSchema,
+                def.Target.Class);
+
+            opts.LeftPrefix = def.Left.Prefix;
+            opts.RightPrefix = def.Right.Prefix;
+            opts.ForceOneToOne = def.JoinSettings.ForceOneToOne;
+            if (def.JoinSettings.SpatialPredicateSpecified)
+                opts.SpatialJoinPredicate = (SpatialOperations)Enum.Parse(typeof(SpatialOperations), def.JoinSettings.SpatialPredicate.ToString());
+
+            return opts;
+        }
+
+        /// <summary>
+        /// Saves the join options to xml
+        /// </summary>
+        /// <param name="opts">The opts.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="file">The file.</param>
+        public void ToXml(FdoJoinOptions opts, string name, string file)
+        {
+            FdoJoinTaskDefinition jdef = new FdoJoinTaskDefinition();
+            jdef.name = name;
+            jdef.JoinSettings = new FdoJoinSettings();
+            if (!string.IsNullOrEmpty(opts.GeometryProperty))
+            {
+                jdef.JoinSettings.DesignatedGeometry = new FdoDesignatedGeometry();
+                jdef.JoinSettings.DesignatedGeometry.Property = opts.GeometryProperty;
+                jdef.JoinSettings.DesignatedGeometry.Side = opts.Side;
+            }
+            List<JoinKey> keys = new List<JoinKey>();
+            foreach (string left in opts.JoinPairs.Keys)
+            {
+                JoinKey key = new JoinKey();
+                key.left = left;
+                key.right = opts.JoinPairs[left];
+                keys.Add(key);
+            }
+            jdef.JoinSettings.JoinKeys = keys.ToArray();
+
+            jdef.Left = new FdoJoinSource();
+            jdef.Left.Class = opts.Left.ClassName;
+            jdef.Left.ConnectionString = opts.Left.Connection.ConnectionString;
+            jdef.Left.FeatureSchema = opts.Left.SchemaName;
+            jdef.Left.Prefix = opts.LeftPrefix;
+            jdef.Left.PropertyList = new List<string>(opts.LeftProperties).ToArray();
+            jdef.Left.Provider = opts.Left.Connection.Provider;
+
+            jdef.Right = new FdoJoinSource();
+            jdef.Right.Class = opts.Right.ClassName;
+            jdef.Right.ConnectionString = opts.Right.Connection.ConnectionString;
+            jdef.Right.FeatureSchema = opts.Right.SchemaName;
+            jdef.Right.Prefix = opts.RightPrefix;
+            jdef.Right.PropertyList = new List<string>(opts.RightProperties).ToArray();
+            jdef.Right.Provider = opts.Right.Connection.Provider;
+
+            jdef.Target = new FdoJoinTarget();
+            jdef.Target.Class = opts.Target.ClassName;
+            jdef.Target.ConnectionString = opts.Target.Connection.ConnectionString;
+            jdef.Target.FeatureSchema = opts.Target.SchemaName;
+            jdef.Target.Provider = opts.Target.Connection.Provider;
+
+            using (XmlTextWriter writer = new XmlTextWriter(file, Encoding.UTF8))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.Indentation = 4;
+
+                XmlSerializer ser = new XmlSerializer(typeof(FdoJoinTaskDefinition));
+                ser.Serialize(writer, jdef);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handler for generating connection names
+    /// </summary>
+    public delegate string ConnectionNameGenerationCallback(int seed);
+
+    /// <summary>
+    /// Helper class for Task Definition serialization
+    /// </summary>
+    public sealed class TaskDefinitionHelper
+    {
+        /// <summary>
+        /// File extension for bulk copy definitions
+        /// </summary>
+        public const string BULKCOPYDEFINITION = ".BulkCopyDefinition";
+        /// <summary>
+        /// File extension for join definitions
+        /// </summary>
+        public const string JOINDEFINITION = ".JoinDefinition";
+
+        /// <summary>
+        /// Determines whether [the specified file] is a definition
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns>
+        /// 	<c>true</c> if [the specified file] is a definition; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsDefinitionFile(string file)
+        {
+            if (!File.Exists(file))
+                return false;
+
+            string ext = Path.GetExtension(file).ToLower();
+
+            return (ext == BULKCOPYDEFINITION.ToLower()) || (ext == JOINDEFINITION.ToLower());
+        }
+
+        /// <summary>
+        /// Determines whether [the specified file] is a bulk copy definition
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns>
+        /// 	<c>true</c> if [the specified file] is a bulk copy definition; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsBulkCopy(string file)
+        {
+            return IsDefinitionFile(file) && (Path.GetExtension(file).ToLower() == BULKCOPYDEFINITION.ToLower());
+        }
+
+        /// <summary>
+        /// Determines whether the specified file is a join definition
+        /// </summary>
+        /// <param name="file">The file.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified file is a join definition; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsJoin(string file)
+        {
+            return IsDefinitionFile(file) && (Path.GetExtension(file).ToLower() == JOINDEFINITION.ToLower());
+        }
+    }
+
+    /// <summary>
+    /// Standalone task definition loader. Use this loader when using only the Core API. 
+    /// Do not use this loader in the context of the FDO Toolbox application.
+    /// </summary>
+    public class DefinitionLoader : BaseDefinitionLoader
+    {
+        /// <summary>
+        /// Creates the connection.
+        /// </summary>
+        /// <param name="provider">The provider.</param>
+        /// <param name="connStr">The conn STR.</param>
+        /// <param name="name">The name that will be assigned to the connection.</param>
+        /// <returns></returns>
+        protected override FdoConnection CreateConnection(string provider, string connStr, ref string name)
+        {
+            return new FdoConnection(provider, connStr);
+        }
+
+        /// <summary>
+        /// Prepares the specified bulk copy definition (freshly deserialized) before the loading process begins
+        /// </summary>
+        /// <param name="def">The bulk copy definition.</param>
+        protected override void Prepare(FdoBulkCopyTaskDefinition def)
+        {
+            
+        }
+    }
+}
---
+++ tags/0.9.4/FdoToolbox/Program.cs
@@ -0,0 +1,194 @@
+#region LGPL Header
+// Copyright (C) 2009, Jackie Ng
+// http://code.google.com/p/fdotoolbox, jumpinjackie@gmail.com
+// 
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+// 
+//
+// See license.txt for more/additional licensing information
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ICSharpCode.Core;
+using System.Reflection;
+using FdoToolbox.Base;
+using System.IO;
+using System.Resources;
+using FdoToolbox.Base.Services;
+using FdoToolbox.Core;
+using System.Threading;
+
+namespace FdoToolbox
+{
+    static class Program
+    {
+        static Mutex appMutex = new Mutex(true, "{E6C2B9BD-6614-409a-9845-BBB23C9539B7}");
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (appMutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(true);
+                Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+
+                // The LoggingService is a small wrapper around log4net.
+                // Our application contains a .config file telling log4net to write
+                // to System.Diagnostics.Trace.
+                LoggingService.Info("Application start");
+
+                AppDomain.CurrentDomain.AssemblyLoad += new AssemblyLoadEventHandler(CurrentDomain_AssemblyLoad);
+
+                // Get a reference to the entry assembly (Startup.exe)
+                Assembly exe = typeof(Program).Assembly;
+
+                // Set the root path of our application. ICSharpCode.Core looks for some other
+                // paths relative to the application root:
+                // "data/resources" for language resources, "data/options" for default options
+                FileUtility.ApplicationRootPath = Path.GetDirectoryName(exe.Location);
+
+                LoggingService.Info("Starting core services...");
+
+                // CoreStartup is a helper class making starting the Core easier.
+                // The parameter is used as the application name, e.g. for the default title of
+                // MessageService.ShowMessage() calls.
+                CoreStartup coreStartup = new CoreStartup("FDO Toolbox");
+                // It is also used as default storage location for the application settings:
+                // "%Application Data%\%Application Name%", but you can override that by setting c.ConfigDirectory
+
+                // Specify the name of the application settings file (.xml is automatically appended)
+                coreStartup.PropertiesName = "AppProperties";
+
+                // Initializes the Core services (ResourceService, PropertyService, etc.)
+                coreStartup.StartCoreServices();
+
+                LoggingService.Info("Looking for AddIns...");
+                // Searches for ".addin" files in the application directory.
+                coreStartup.AddAddInsFromDirectory(Path.Combine(FileUtility.ApplicationRootPath, "AddIns"));
+
+                // Searches for a "AddIns.xml" in the user profile that specifies the names of the
+                // add-ins that were deactivated by the user, and adds "external" AddIns.
+                coreStartup.ConfigureExternalAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddIns.xml"));
+
+                // Searches for add-ins installed by the user into his profile directory. This also
+                // performs the job of installing, uninstalling or upgrading add-ins if the user
+                // requested it the last time this application was running.
+                coreStartup.ConfigureUserAddIns(Path.Combine(PropertyService.ConfigDirectory, "AddInInstallTemp"),
+                                                Path.Combine(PropertyService.ConfigDirectory, "AddIns"));
+
+                LoggingService.Info("Checking FDO");
+                // Set FDO path
+                string fdoPath = Preferences.FdoPath;
+                bool abort = false;
+                if (!FdoAssemblyResolver.IsValidFdoPath(fdoPath))
+                {
+                    fdoPath = Path.Combine(FileUtility.ApplicationRootPath, "FDO");
+                    Preferences.FdoPath = fdoPath;
+
+                    while (!FdoAssemblyResolver.IsValidFdoPath(fdoPath) && !abort)
+                    {
+                        FolderBrowserDialog fb = new FolderBrowserDialog();
+                        fb.Description = "Select the directory that contains the FDO binaries";
+                        if (fb.ShowDialog() == DialogResult.Cancel)
+                        {
+                            abort = true;
+                        }
+                        else
+                        {
+                            fdoPath = fb.SelectedPath;
+                            Preferences.FdoPath = fdoPath;
+                        }
+                    }
+                }
+
+                if (abort)
+                    return;
+
+                AddInAssemblyResolver.RegisterLibraries(
+                    fdoPath, 
+                    "OSGeo.FDO.dll", 
+                    "OSGeo.FDO.Common.dll", 
+                    "OSGeo.FDO.Geometry.dll", 
+                    "OSGeo.FDO.Spatial.dll",
+                    "OSGeo.FDO.Providers.MySQL.Overrides.dll",
+                    "OSGeo.FDO.Providers.ODBC.Overrides.dll",
+                    "OSGeo.FDO.Providers.Rdbms.dll",
+                    "OSGeo.FDO.Providers.Rdbms.Overrides.dll",
+                    "OSGeo.FDO.Providers.SHP.Overrides.dll",
+                    "OSGeo.FDO.Providers.SQLServerSpatial.Overrides.dll",
+                    "OSGeo.FDO.Providers.WMS.Overrides.dll");
+
+                LoggingService.Info("FDO path set to: " + fdoPath);
+
+                LoggingService.Info("Loading AddInTree...");
+                // Now finally initialize the application. This parses the ".addin" files and
+                // creates the AddIn tree. It also automatically runs the commands in
+                // "/Workspace/Autostart"
+                coreStartup.RunInitialization();
+
+                LoggingService.Info("Initializing Workbench...");
+                // Workbench is our class from the base project, this method creates an instance
+                // of the main form.
+                log4net.Config.XmlConfigurator.Configure();
+                Workbench.InitializeWorkbench();
+
+                try
+                {
+                    LoggingService.Info("Running application...");
+                    // Workbench.Instance is the instance of the main form, run the message loop.
+                    Application.Run(Workbench.Instance);
+                }
+                finally
+                {
+                    try
+                    {
+                        // Save changed properties
+                        PropertyService.Save();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageService.ShowError(ex, "Error storing properties");
+                    }
+                }
+                LoggingService.Info("Application shutdown");
+                appMutex.ReleaseMutex();
+            }
+            else
+            {
+                //Send our message to make the workbench be the topmost form
+                NativeMethods.PostMessage(
+                    (IntPtr)NativeMethods.HWND_BROADCAST,
+                    NativeMethods.WM_SHOWME,
+                    IntPtr.Zero,
+                    IntPtr.Zero);
+            }
+        }
+
+        static void CurrentDomain_AssemblyLoad(object sender, AssemblyLoadEventArgs args)
+        {
+            LoggingService.InfoFormatted("Loaded assembly: {0}", args.LoadedAssembly.GetName().Name);
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageService.ShowError(e.Exception);
+        }
+    }
+}
